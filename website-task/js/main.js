a64d6280aab434a74235f4ff5cbba39449a4bb7b
Manu <90511900+Manumanoharan@users.noreply.github.com>
2023-02-28 13:49:59 +0530

    website-task

---
+++ website-task/js/main.js
@@ -0,0 +1,305 @@
+
+
+function animationIn1() {
+    document.getElementById('ani-1').style.transition = 'all 0.5s linear'
+    document.getElementById('ani-1').style.transform = 'scale(1)'
+}
+function animationOut1() {
+    document.getElementById('ani-1').style.transition = 'all 0.5s linear'
+    document.getElementById('ani-1').style.transform = 'scale(0)'
+}
+function animationIn2() {
+    document.getElementById('ani-2').style.transition = 'all 0.5s linear'
+    document.getElementById('ani-2').style.transform = 'scale(1)'
+}
+function animationOut2() {
+    document.getElementById('ani-2').style.transition = 'all 0.5s linear'
+    document.getElementById('ani-2').style.transform = 'scale(0)'
+}
+function animationIn3() {
+    document.getElementById('ani-3').style.transition = 'all 0.5s linear'
+    document.getElementById('ani-3').style.transform = 'scale(1)'
+}
+function animationOut3() {
+    document.getElementById('ani-3').style.transition = 'all 0.5s linear'
+    document.getElementById('ani-3').style.transform = 'scale(0)'
+}
+function animationIn4() {
+    document.getElementById('ani-4').style.transition = 'all 0.5s linear'
+    document.getElementById('ani-4').style.transform = 'scale(1)'
+}
+function animationOut4() {
+    document.getElementById('ani-4').style.transition = 'all 0.5s linear'
+    document.getElementById('ani-4').style.transform = 'scale(0)'
+}
+function animationIn5() {
+    document.getElementById('ani-5').style.transition = 'all 0.5s linear'
+    document.getElementById('ani-5').style.transform = 'scale(1)'
+}
+function animationOut5() {
+    document.getElementById('ani-5').style.transition = 'all 0.5s linear'
+    document.getElementById('ani-5').style.transform = 'scale(0)'
+}
+function animationIn6() {
+    document.getElementById('ani-6').style.transition = 'all 0.5s linear'
+    document.getElementById('ani-6').style.transform = 'scale(1)'
+}
+function animationOut6() {
+    document.getElementById('ani-6').style.transition = 'all 0.5s linear'
+    document.getElementById('ani-6').style.transform = 'scale(0)'
+}
+
+
+function aniArrow1() {
+    document.getElementById('uparrow-1').style.transition = 'all 0.5s linear'
+    document.getElementById('uparrow-1').style.transform = 'scale(1)'
+    document.getElementById('uparrow-1').style.transformOrigin= 'bottom'
+
+}
+function aniArrowOut1() {
+    document.getElementById('uparrow-1').style.transition = 'all 0.5s linear'
+    document.getElementById('uparrow-1').style.transform = 'scale(0)'
+}
+function aniArrow2() {
+    document.getElementById('uparrow-2').style.transition = 'all 0.5s linear'
+    document.getElementById('uparrow-2').style.transform = 'scale(1)'
+    document.getElementById('uparrow-2').style.transformOrigin= 'bottom'
+
+}
+function aniArrowOut2() {
+    document.getElementById('uparrow-2').style.transition = 'all 0.5s linear'
+    document.getElementById('uparrow-2').style.transform = 'scale(0)'
+}
+function aniArrow3() {
+    document.getElementById('uparrow-3').style.transition = 'all 0.5s linear'
+    document.getElementById('uparrow-3').style.transform = 'scale(1)'
+    document.getElementById('uparrow-3').style.transformOrigin= 'bottom'
+
+}
+function aniArrowOut3() {
+    document.getElementById('uparrow-3').style.transition = 'all 0.5s linear'
+    document.getElementById('uparrow-3').style.transform = 'scale(0)'
+}
+function aniArrow4() {
+    document.getEementById('uparrow-4').style.transition = 'all 0.5s linear'
+    document.getElementById('uparrow-4').style.transform = 'scale(1)'
+    document.getElementById('uparrow-4').style.transformOrigin= 'bottom'
+
+}
+function aniArrowOut4() {
+    document.getElementById('uparrow-4').style.transition = 'all 0.5s linear'
+    document.getElementById('uparrow-4').style.transform = 'scale(0)'
+}
+function aniArrow5() {
+    document.getElementById('uparrow-5').style.transition = 'all 0.5s linear'
+    document.getElementById('uparrow-5').style.transform = 'scale(1)'
+    document.getElementById('uparrow-5').style.transformOrigin= 'bottom'
+}
+function aniArrowOut5() {
+    document.getElementById('uparrow-5').style.transition = 'all 0.5s linear'
+    document.getElementById('uparrow-5').style.transform = 'scale(0)'
+}
+
+function imgzoom1(){
+    document.getElementById('img-change').style.transform = 'scale(1.1)'
+    document.getElementById('img-change').style.transition = 'all 0.5s linear'
+}
+function imgout1(){
+    document.getElementById('img-change').style.transform = 'scale(1)'
+}
+function imgzoom2(){
+    document.getElementById('img-change-1').style.transition = 'all 0.5s linear'
+    document.getElementById('img-change-1').style.transform = 'scale(1.1)'
+}
+function imgout2(){
+    document.getElementById('img-change-1').style.transform = 'scale(1)'
+}
+function imgzoom3(){
+    document.getElementById('img-change-2').style.transform = 'scale(1.1)'
+    document.getElementById('img-change-2').style.transition = 'all 0.5s linear'
+}
+function imgout3(){
+    document.getElementById('img-change-2').style.transform = 'scale(1)'
+}
+
+function webZoom(){
+    document.getElementById('web-img-zoom').style.transform = 'scale(1.1)'
+    document.getElementById('web-img-zoom').style.transition = 'all 0.5s linear'
+}
+function webOut(){
+    document.getElementById('web-img-zoom').style.transform = 'scale(1)'
+}
+function webZoom1(){
+    document.getElementById('web-img-zoom1').style.transform = 'scale(1.1)'
+    document.getElementById('web-img-zoom1').style.transition = 'all 0.5s linear'
+}
+function webOut1(){
+    document.getElementById('web-img-zoom1').style.transform = 'scale(1)'
+}
+function webZoom2(){
+    document.getElementById('web-img-zoom2').style.transform = 'scale(1.1)'
+    document.getElementById('web-img-zoom2').style.transition = 'all 0.5s linear'
+}
+function webOut2(){
+    document.getElementById('web-img-zoom2').style.transform = 'scale(1)'
+}
+function webZoom2(){
+    document.getElementById('web-img-zoom2').style.transform = 'scale(1.1)'
+    document.getElementById('web-img-zoom2').style.transition = 'all 0.5s linear'
+}
+function webOut2(){
+    document.getElementById('web-img-zoom2').style.transform = 'scale(1)'
+}
+function arrowMove1(){
+    document.getElementById('arrow-right-1').style.transform = 'translateX(15px)'
+    document.getElementById('arrow-right-1').style.transition = 'all 0.3s linear'
+}
+function arrowOut1(){
+    document.getElementById('arrow-right-1').style.transform = 'translateX(0)'
+}
+function arrowMove2(){
+    document.getElementById('arrow-right-2').style.transform = 'translateX(15px)'
+    document.getElementById('arrow-right-2').style.transition = 'all 0.3s linear'
+}
+function arrowOut2(){
+    document.getElementById('arrow-right-2').style.transform = 'translateX(0)'
+}
+function arrowMove3(){
+    document.getElementById('arrow-right-3').style.transform = 'translateX(15px)'
+    document.getElementById('arrow-right-3').style.transition = 'all 0.3s linear'
+}
+function arrowOut3(){
+    document.getElementById('arrow-right-3').style.transform = 'translateX(0)'
+}
+function arrowMove4(){
+    document.getElementById('arrow-right-4').style.transform = 'translateX(15px)'
+    document.getElementById('arrow-right-4').style.transition = 'all 0.3s linear'
+}
+function arrowOut4(){
+    document.getElementById('arrow-right-4').style.transform = 'translateX(0)'
+}
+function arrowMove5(){
+    document.getElementById('arrow-right-5').style.transform = 'translateX(15px)'
+    document.getElementById('arrow-right-5').style.transition = 'all 0.3s linear'
+}
+function arrowOut5(){
+    document.getElementById('arrow-right-5').style.transform = 'translateX(0)'
+}
+function arrowMove6(){
+    document.getElementById('arrow-right-6').style.transform = 'translateX(15px)'
+    document.getElementById('arrow-right-6').style.transition = 'all 0.3s linear'
+}
+function arrowOut6(){
+    document.getElementById('arrow-right-6').style.transform = 'translateX(0)'
+}
+function arrowMove7(){
+    document.getElementById('arrow-right-7').style.transform = 'translateX(15px)'
+    document.getElementById('arrow-right-7').style.transition = 'all 0.3s linear'
+}
+function arrowOut7(){
+    document.getElementById('arrow-right-7').style.transform = 'translateX(0)'
+}
+function arrowMove8(){
+    document.getElementById('arrow-right-8').style.transform = 'translateX(15px)'
+    document.getElementById('arrow-right-8').style.transition = 'all 0.3s linear'
+}
+function arrowOut8(){
+    document.getElementById('arrow-right-8').style.transform = 'translateX(0)'
+}
+function arrowMove9(){
+    document.getElementById('arrow-right-9').style.transform = 'translateX(15px)'
+    document.getElementById('arrow-right-9').style.transition = 'all 0.3s linear'
+}
+function arrowOut9(){
+    document.getElementById('arrow-right-9').style.transform = 'translateX(0)'
+}
+
+function arrowMove10(){
+    document.getElementById('arrow-right-10').style.transform = 'translateX(15px)'
+    document.getElementById('arrow-right-10').style.transition = 'all 0.3s linear'
+}
+function arrowOut10(){
+    document.getElementById('arrow-right-10').style.transform = 'translateX(0)'
+}
+function arrowMove11(){
+    document.getElementById('arrow-right-11').style.transform = 'translateX(15px)'
+    document.getElementById('arrow-right-11').style.transition = 'all 0.3s linear'
+}
+function arrowOut11(){
+    document.getElementById('arrow-right-11').style.transform = 'translateX(0)'
+}
+function arrowMove12(){
+    document.getElementById('arrow-right-12').style.transform = 'translateX(15px)'
+    document.getElementById('arrow-right-12').style.transition = 'all 0.3s linear'
+}
+function arrowOut12(){
+    document.getElementById('arrow-right-12').style.transform = 'translateX(0)'
+}
+
+
+var mybutton=document.getElementById("cta-arrow-fix")
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+mybutton.style.display = "block";
+} else {
+mybutton.style.display = "none";
+}
+myFunction()
+// scrollAni()
+}
+// function scrollAni(){
+//     if (document.body.scrollTop > 500 || document.documentElement.scrollTop > 500) {
+//         document.getElementById('enqry-2').style.classList.add('scales') 
+//         } else {
+//         document.getElementById('enqry-2').style.classList.remove('scales')
+//         }
+// }
+
+function topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+  }
+
+//   window.onscroll = function() {myFunction()};
+var animation=document.getElementById("count-1");
+var navbar = document.getElementById("nav-2");
+var colr = document.getElementById("color-change-1");
+var colr1 = document.getElementById("color-change-2");
+var colr2 = document.getElementById("color-change-3");
+var colr3 = document.getElementById("blogs");
+var colr4 = document.getElementById("pag");
+var colr5 = document.getElementById("color-change-4");
+var starts = document.getElementById("start-logo");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("nav-fixed")
+    colr.classList.add("change-color-1")
+    colr1.classList.add("change-color-1")
+    colr2.classList.add("change-color-1")
+    colr3.classList.add("change-color-1")
+    colr4.classList.add("change-color-1")
+    colr5.classList.add("change-color-1")
+    starts.classList.add("change-color-2")
+    
+  } else {
+    navbar.classList.remove("nav-fixed");
+    colr.classList.remove("change-color-1")
+    colr1.classList.remove("change-color-1")
+    colr2.classList.remove("change-color-1")
+    colr3.classList.remove("change-color-1")
+    colr4.classList.remove("change-color-1")
+    colr5.classList.remove("change-color-1")
+    starts.classList.remove("change-color-2")
+  }
+}
+// var re=document.getElementById("creat")
+// console.log(re)
+
+// function click(){
+//     re.classList.add("down")
+// }
+click();
